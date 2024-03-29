ced563303ac62fafb5b748bf01d27caf5c223b7a
Fadhil <fadhilsaheer@gmail.com>
2024-03-29 23:14:34 +0530

    feat: audio playing & duration calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{OutputStream};
+use lofty::{AudioFile};
+
 fn main() {
-    println!("Hello, world!");
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let audio_file = File::open("audio.mp3").unwrap();
+    let file = lofty::Probe::open("audio.mp3").unwrap();
+
+    let tagged_file = file.read().unwrap();
+    let duration = tagged_file.properties().duration().as_secs_f64();
+
+    let reader: BufReader<File> = BufReader::new(audio_file);
+
+
+    let sink = stream_handle.play_once(reader).unwrap();
+
+
+    println!("Duration - {}", duration);
+
+    std::thread::sleep(std::time::Duration::from_secs(duration as u64 + 1));
 }
