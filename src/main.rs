43884c9cea517d338253988185dede4e7e479375
Fadhil <fadhilsaheer@gmail.com>
2024-03-30 19:32:52 +0530

    feat: file source

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 use std::fs::File;
-use std::io::BufReader;
-use rodio::{OutputStream};
+use std::sync::{Arc, Mutex};
+use rodio::{Decoder, OutputStream, Sink};
 use lofty::{AudioFile};
+
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    let audio_file = File::open("audio.mp3").unwrap();
-    let file = lofty::Probe::open("audio.mp3").unwrap();
+    // open file
+    let mut audio_file = File::open("audio.mp3").unwrap();
 
-    let tagged_file = file.read().unwrap();
+    // get duration
+    let tagged_file = lofty::read_from(&mut audio_file).unwrap();
     let duration = tagged_file.properties().duration().as_secs_f64();
 
-    let reader: BufReader<File> = BufReader::new(audio_file);
 
 
-    let sink = stream_handle.play_once(reader).unwrap();
+    // let source_original = Decoder::new(audio_file).unwrap();
+    // let source = Arc::new(source_original);
+
+    let source = Decoder::new(audio_file).unwrap();
+
+    // let file = lofty::Probe::open("audio.mp3").unwrap();
+    // let source = Decoder::new(audio_file).unwrap();
+
+    // let tagged_file = file.read().unwrap();
+    // let duration = tagged_file.properties().duration().as_secs_f64();
+
+    // let reader: BufReader<File> = BufReader::new(audio_file);
 
 
-    println!("Duration - {}", duration);
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.append(source);
 
-    std::thread::sleep(std::time::Duration::from_secs(duration as u64 + 1));
+    source.skip_duration(std::time::Duration::from_secs(5));
+
+
+    sink.sleep_until_end();
+
+    // std::thread::sleep(std::time::Duration::from_secs(duration as u64 + 1 - 7));
 }
